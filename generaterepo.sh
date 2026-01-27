b51aafb93a431e87a5d379f3bf89e9f77fe5a983
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-01-27 14:16:09 -0300

    Update reponames

--- generaterepo.sh
+++ generaterepo.sh
@@ -17,7 +17,7 @@ expect -c "spawn gpg2 --edit-key 307E04B769840811099F4077ED5D59DA704DEBE2 trust 
 
 # generation of new deb repository
 echo 'VasakOS: Inicio de Configuracion del Repo'
-reprepro -V -b pkgs/debs includedeb patojad debinstall/*deb
+reprepro -V -b pkgs/debs includedeb vasakos debinstall/*deb
 echo 'VasakOS: Fin de Configuracion del Repo'
 
 ls pkgs/debs/dists/patojad/main
